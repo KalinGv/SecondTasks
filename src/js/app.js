aa0c049d62897738da6e6dcbbe1f52a053bf8bb9
KalinGv <KalinGv68@gmail.com>
2019-04-16 15:38:03 +0200

    Exported more modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+//imports from other files
 import '../css/main.scss';
 import '../css/input-elements.scss';
-
 import {secretButton, secretParagraph} from './dom-loader';
 
+//making the secret message to apper by clicking the button
 var showSecret = false;
 
 secretButton.addEventListener('click', toggleSecretState);
--- src/js/dom-loader.js
+++ src/js/dom-loader.js
@@ -1,2 +1,4 @@
 export var secretButton = document.querySelector('#secret-button');
 export var secretParagraph = document.querySelector('#secret-paragraph');
+
+//Taking html elements into a variables and exporting them to be used in app.js
